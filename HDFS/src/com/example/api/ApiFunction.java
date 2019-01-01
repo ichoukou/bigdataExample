efa3401bc4443cd33b311142eb827e73c066777e
tanmintao <154411492@qq.com>
2019-01-01 16:51:26 +0800

    更新

--- HDFS/src/com/example/api/ApiFunction.java
+++ HDFS/src/com/example/api/ApiFunction.java
@@ -23,6 +23,7 @@ public class ApiFunction {
 	 */
 	public void initHDFS() throws Exception {
 		
+		
 		// 1 创建配置信息对象
 		// new Configuration();的时候，它就会去加载jar包中的hdfs-default.xml
 		// 然后再加载classpath下的hdfs-site.xml
